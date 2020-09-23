ca47f8f66d113a18d06f5d82d2118cc00a06e14b
mdfazal <fazalmd964@gmail.com>
2020-09-23 23:34:01 +0530

    todo

--- JS/todo.js
+++ JS/todo.js
@@ -6,11 +6,15 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
     if(input === "list"){
-        console.log(todo);
+        console.log("*******");
+        todo.forEach(function(todo, i){
+            console.log(i + ": "+todo);
+        });
+        
     } else if (input === "new") {
         var newTodo = prompt("Enter new todo");
         todo.push(newTodo);
     }
     input = prompt("What would you like to do?");
 }
-console.log("ok you quit ");
+console.log("ok you quit "); 
