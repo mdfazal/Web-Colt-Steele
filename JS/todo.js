4ecdf1df648aa067539826ce93ab79a0e58722b2
mdfazal <fazalmd964@gmail.com>
2020-09-23 18:18:53 +0530

    todo

--- JS/todo.js
+++ JS/todo.js
@@ -1,3 +1,7 @@
-var todo = [];
+var todo = ["Buy new Turtle"];
 
-var input = prompt("What would you like to do?")
+var input = prompt("What would you like to do?");
+
+if(input === "list"){
+    console.log(todo);
+}
