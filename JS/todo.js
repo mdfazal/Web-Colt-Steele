60300dd7c3f3ce215e533ee803386a10654e8161
mdfazal <fazalmd964@gmail.com>
2020-09-23 18:35:36 +0530

    todo

--- JS/todo.js
+++ JS/todo.js
@@ -4,4 +4,7 @@ var input = prompt("What would you like to do?");
 
 if(input === "list"){
     console.log(todo);
+} else if (input === "new") {
+    var newTodo = prompt("Enter new todo");
+    todo.push(newTodo);
 }
