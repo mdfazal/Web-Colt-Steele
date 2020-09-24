af9b171466671a2d4e145aaaaf82c6b1ef6d76f4
mdfazal <fazalmd964@gmail.com>
2020-09-24 12:09:55 +0530

    challenge

--- JS/challenge1.js
+++ JS/challenge1.js
@@ -34,3 +34,27 @@ function max(arr){
     }
     return ele;
 }
+var movies = [
+    {
+        title:"In Bruges",
+        hasWatched: true,
+        rating: 5
+    },
+    {
+        title: "Frozen",
+        hasWatched: false,
+        rating: 4.5
+    } 
+]
+movies.forEach(function(movie){
+    var result = "You Have ";
+    if(movie.hasWatched){
+        result += "watched";
+    } else {
+        result += "not seen";
+    }
+    result += "\""+movie.title+"\" - ";
+    result += movie.rating + " stars";
+    console.log(result)
+    }
+);
