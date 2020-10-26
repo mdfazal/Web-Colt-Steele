cf411639dc58e0151678ed324f9589d7aaad48cb
mdfazal <fazalmd964@gmail.com>
2020-10-26 23:39:31 +0530

    score keeper added

--- score keeper/script.js
+++ score keeper/script.js
@@ -1,5 +1,6 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var p1Score=0;
@@ -11,6 +12,7 @@ p1Button.addEventListener("click", function(){
   if(!gameOver){
     p1Score++;
     if(p1Score === winningScore){
+      p1Display.classList.add("winner");
       gameOver = true;
     }
     p1Display.textContent = p1Score;
@@ -22,8 +24,11 @@ p2Button.addEventListener("click", function(){
   if(!gameOver){
     p2Score++;
     if(p2Score === winningScore){
+      p2Display.classList.add("winner");
       gameOver = true;
     }
     p2Display.textContent = p2Score;
   }
 });
+
+console.log(resetButton);
