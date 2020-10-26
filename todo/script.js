b2df6d99a851b7d73792c063b43c349429804a7c
mdfazal <fazalmd964@gmail.com>
2020-10-27 00:32:35 +0530

    todo events added

--- todo/script.js
+++ todo/script.js
@@ -1,15 +1,14 @@
 var lis = document.querySelectorAll("li");
 
 for(var i =0; i<lis.length;i++){
-  lis[i]addEventListener("mouseover", function(){
+  lis[i].addEventListener("mouseover", function(){
     this.style.color = "green";
+  });
+  lis[i].addEventListener("mouseout", function(){
+    this.style.color = "black";
+  });
+  lis[i].addEventListener("click". function(){
+    this.classList.toggle("done");
   });
 }
 
-firstLI.addEventListener("mouseover", function(){
-  firstLI.style.color = "green";
-});
-
-firstLI.addEventListener("mouseout", function(){
-  firstLI.style.color = "black";
-});
