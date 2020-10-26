00bece8f68d86fd96748fa82abbd0f7bdde9101e
mdfazal <fazalmd964@gmail.com>
2020-10-26 23:15:36 +0530

    score keeper added

--- score keeper/script.js
+++ score keeper/script.js
@@ -1,2 +1,6 @@
-var p1button = document.querySelector("#p1");
-var p2button = document.getElementById("p1");
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p1");
+
+p1Button.addEventListener("click", function(){
+  alert("clicked");
+});
