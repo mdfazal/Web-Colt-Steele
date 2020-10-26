6da0b552d5e101de1d36b8719bc80b7e4fbca8c9
mdfazal <fazalmd964@gmail.com>
2020-10-27 00:13:49 +0530

    score keeper added

--- score keeper/script.js
+++ score keeper/script.js
@@ -4,6 +4,7 @@ var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score=0;
 var p2Score=0;
 var gameOver = false;
@@ -33,6 +34,10 @@ p2Button.addEventListener("click", function(){
 });
 
 resetButton.addEventListener("click", function(){
+  reset();
+});
+
+function reset(){
   p1Score = 0;
   p2Score = 0;
   p1Display.textContent = 0;
@@ -40,9 +45,10 @@ resetButton.addEventListener("click", function(){
   p1Display.classList.remove("winner");
   p2Display.classList.remove("winner");
   gameOver = false;
-
-})
+}
 
 numInput.addEventListener("change", function(){
-  alert("Changed the Input");
+  winningScoreDisplay.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
 });
