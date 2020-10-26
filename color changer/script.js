4f376a38e8e3e054d86645a47cb5bc8a0562e720
mdfazal <fazalmd964@gmail.com>
2020-10-26 23:03:41 +0530

    color changer added

--- color changer/script.js
+++ color changer/script.js
@@ -1,17 +1,23 @@
 var button = document.querySelector("button");
-var isPurple = false;
+//var isPurple = false;
 
 button.addEventListener("click", function(){
-    if(isPurple){
-        document.body.style.background = "white";
+    document.body.classList.toggle("prurple");
+});
+
+
+
+//button.addEventListener("click", function(){
+  //  if(isPurple){
+    //    document.body.style.background = "white";
         
-    } else {
-        document.body.style.background = "purple";
+    //} else {
+      //  document.body.style.background = "purple";
         
-    }
-    isPurple = !isPurple;
+    //}
+    //isPurple = !isPurple;
     //if white
     //make it purple
     //else
     //make it white
-});
+//});
