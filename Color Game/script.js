1a2d94c66f788c5ff76aaf6ca06152d4ebb24815
mdfazal <fazalmd964@gmail.com>
2020-10-27 20:19:37 +0530

    color game added

--- Color Game/script.js
+++ Color Game/script.js
@@ -8,7 +8,27 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i =0;i<squares.length;i++){
+  //add initial colors to the squares
   squares[i].style.backgroundColor = colors[i];
+
+  //add click listener to squares
+  squares[i].addEventListener("click", function(){
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if(clickedColor === pickedColor ){
+      alert("Correct");
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
+
 }
