df53b5246b7c77c33dee556a2b314bd543365cab
mdfazal <fazalmd964@gmail.com>
2020-10-29 20:19:15 +0530

    jQuery added

---
+++ Jquery demo/script.js
@@ -0,0 +1,23 @@
+var button = document.querySelector("button");
+//var isPurple = false;
+
+button.addEventListener("click", function(){
+    document.body.classList.toggle("prurple");
+});
+
+
+
+//button.addEventListener("click", function(){
+  //  if(isPurple){
+    //    document.body.style.background = "white";
+        
+    //} else {
+      //  document.body.style.background = "purple";
+        
+    //}
+    //isPurple = !isPurple;
+    //if white
+    //make it purple
+    //else
+    //make it white
+//});
